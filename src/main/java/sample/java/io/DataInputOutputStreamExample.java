05bbf95cee1621785efaf2853c2043072839342b
kopelevi <adi.kopelevich@hp.com>
2015-11-05 09:55:34 +0200

    add int and writer,flush to ensure wrtiing to disk (otherwise might hang in cache)

--- src/main/java/sample/java/io/DataInputOutputStreamExample.java
+++ src/main/java/sample/java/io/DataInputOutputStreamExample.java
@@ -15,7 +15,9 @@ public class DataInputOutputStreamExample {
              DataOutputStream dataOutput = new DataOutputStream(outputStream)) {
             dataOutput.writeBoolean(true);
             dataOutput.writeFloat(123.45F);
+            dataOutput.writeInt(789);
             dataOutput.writeLong(789);
+            dataOutput.flush();
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         } catch (IOException e) {
@@ -26,6 +28,7 @@ public class DataInputOutputStreamExample {
              DataInputStream dataInput = new DataInputStream(inputStream)) {
             System.out.println(dataInput.readBoolean());
             System.out.println(dataInput.readFloat());
+            System.out.println(dataInput.readInt());
             System.out.println(dataInput.readLong());
         } catch (FileNotFoundException e) {
             e.printStackTrace();
